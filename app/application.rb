145cdc44bd0b149cc42ea60d47882aa9e04710f5
Peter Kim <peterckim96@gmail.com>
2018-07-10 01:19:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,9 +7,7 @@ class Application
       item_name = req.path.split("/items/").last
       
       if Item.all.include?(item_name)
-        item = Item.all.find do |i|
-          i.name == item_name
-        end
+        item = Item.all.find { |i| i.name == item_name }
         resp.write "#{item.price}"
         resp.status = 200
       else
