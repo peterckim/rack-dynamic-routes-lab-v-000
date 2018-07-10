657f138364f442871ee0855c87b8b6d85354888c
Peter Kim <peterckim96@gmail.com>
2018-07-10 01:10:26 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+  @@items = ["Apples", "Carrots", "Pears"]
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_to_add = req.params["item"]
+      @@items.each do |item|
+        if item.name = item_to_add
+          return item.price
+        end
+      end
+      resp.write "Item not found"
+      resp.status = 400
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    
+    resp.finish
+  end
+end
